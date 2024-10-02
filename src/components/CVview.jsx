4d0b5c29b633ffa0940b833ab9470f796eb0c620
Jonathan Dawson <jonnydawson@hotmail.com>
2024-10-02 09:44:06 +0100

    Created p elements in CV view to test

--- src/components/CVview.jsx
+++ src/components/CVview.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 
-export default function CVview({personalInfo}) {
+export default function CVview({ personalInfo, eduExperience }) {
     return <>
     <p>{personalInfo['First Name']}</p>
+    <p>{eduExperience[0]['School']}</p>
+    <p>{eduExperience[0]['Course']}</p>
+    <p>{eduExperience[0]['Date']}</p>
+    {eduExperience[1] && <p>{eduExperience[1]['School']}</p>}
     </>
 }
