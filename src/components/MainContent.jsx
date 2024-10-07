d390e333ddce4775537e19fd8c016db64cf8e7bd
Jonathan Dawson <jonnydawson@hotmail.com>
2024-10-07 09:48:09 +0100

    Added hide and edit section buttons

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -10,20 +10,36 @@ export default function MainContent() {
     const [currentPersonalInfo, setPersonalInfo] = useState(cvData['Personal Details']);
     const [currentEduExperience, setEduExperience] = useState(cvData['Educational Experience']);
     const [currentWorkExperience, setWorkExperience] = useState(cvData['Practical Experience']);
+    const [currentSection, setSection] = useState(0)
+
+
 
     return <>
     <section>
+        
         <section>
             <h2>Personal Details:</h2>
+            {currentSection === 1 && <>
             <Personalinfo personalInfo={currentPersonalInfo} setPersonalInfo={setPersonalInfo}/>
+            <button onClick={() => {setSection(0)}}>Hide</button>
+            </>}
+            {currentSection !== 1 && <button onClick={() => {setSection(1)}}>Edit</button>}
         </section>
         <section>
             <h2>Education:</h2>
+            {currentSection === 2 && <>
             <Eduexperience eduExperience={currentEduExperience} setEduExperience={setEduExperience}/>
+            <button onClick={() => {setSection(0)}}>Hide</button>
+            </>}
+            {currentSection !== 2 && <button onClick={() => {setSection(2)}}>Edit</button>}
         </section>
         <section>
             <h2>Work Experience:</h2>
+            {currentSection === 3 && <>
             <Jobexperience workExperience={currentWorkExperience} setWorkExperience={setWorkExperience}/>
+            <button onClick={() => {setSection(0)}}>Hide</button>
+            </>}
+            {currentSection !== 3 && <button onClick={() => {setSection(3)}}>Edit</button>}
         </section>
     </section>
     <section>
