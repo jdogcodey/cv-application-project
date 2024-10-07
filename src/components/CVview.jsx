69cce1a4c375a205d8310c1446f245c36272dfcc
Jonathan Dawson <jonnydawson@hotmail.com>
2024-10-07 11:20:52 +0100

    Added work date function format

--- src/components/CVview.jsx
+++ src/components/CVview.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { format } from 'date-fns';
+import { format, formatDistance } from 'date-fns';
 
 export default function CVview({ personalInfo, eduExperience, workExperience }) {
 
@@ -9,6 +9,18 @@ export default function CVview({ personalInfo, eduExperience, workExperience }) 
         return formattedDate;
     }
 
+    function workDate(x, y) {
+        const startDate = new Date(x);
+        const finishDate = new Date(y);
+
+        const formattedStart = format(startDate, 'dd/MM/yy');
+        const formattedEnd = format(finishDate, 'dd/MM/yy');
+        const length = formatDistance(x, y);
+        const totalDateOutput = `${formattedStart}-${formattedEnd} | ${length}`
+
+        return totalDateOutput;
+    }
+
     return <>
     <section>
         <h2>{`${personalInfo['First Name']} ${personalInfo['Surname']}`}</h2>
@@ -16,9 +28,18 @@ export default function CVview({ personalInfo, eduExperience, workExperience }) 
     </section>
     <section>
         {eduExperience.map((item, index) => (
-            <div key={item}>
+            <div key={item + index}>
                 <h3>{item['School']}</h3>
                 <p>{`${item['Course']} • ${courseDate(item['Date'])}`}</p>
+            </div>
+        ))}
+    </section>
+    <section>
+        {workExperience.map((item, index) => (
+            <div key={item + index}>
+                <h3>{item['Position']}</h3>
+                <h4>{item['Company']}</h4>
+                <p>{workDate(item['Date-start'], item['Date-finish'])}</p>
             </div>
         ))}
     </section>
