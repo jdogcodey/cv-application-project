1c212dd241be6e8edffe86de57ad464194ae0b12
Jonathan Dawson <jonnydawson@hotmail.com>
2024-10-07 15:18:42 +0100

    Nav bar design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return <header>
         <h1>CV Automator!</h1>
         <nav>
-            <button onClick={() => {window.open(`https://github.com/jdogcodey`, '_blank')}}><img src='/images/github.svg'/></button>
+            <button id='github-logo' onClick={() => {window.open(`https://github.com/jdogcodey`, '_blank')}}><img src='/images/github.svg'/></button>
         </nav>
     </header>
 }
